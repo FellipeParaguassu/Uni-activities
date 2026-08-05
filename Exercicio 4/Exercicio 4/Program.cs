3447c860cdbcb7222038e90e66ed806a718d6c89
FellipeParaguassu <8rrfellipeparaguassu@gmail.com>
2026-08-05 17:51:06 -0400

    Exercício 3 de C#

---
+++ Exercicio 4/Exercicio 4/Program.cs
@@ -0,0 +1,5 @@
+﻿//Conversor Celsius para Fahreinheit
+Console.WriteLine("Sistema de Celsius para Fahrenheit \nQuantos graus Celsius?");
+double celsius = double.Parse(Console.ReadLine());
+double fahrenheit = (celsius * 9/5) + 32;
+Console.WriteLine($"{celsius:F2} graus Celsius equivalem a {fahrenheit:F2} graus Fahrenheit.");
