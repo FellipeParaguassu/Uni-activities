6d8a4bce46b8a82d64ccf30f655dbb70ade7f87c
FellipeParaguassu <8rrfellipeparaguassu@gmail.com>
2026-08-05 16:49:10 -0400

    Exercício 2 em C#

---
+++ Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,7 @@
+﻿//Cálculo de IMC usando altura e peso usando double para arredondar
+Console.WriteLine("Qual seu peso?");
+double peso = double.Parse(Console.ReadLine());
+Console.WriteLine("Qual sua Altura?");
+double altura = double.Parse(Console.ReadLine());
+double IMC = peso / (altura * altura);
+Console.WriteLine($"Se sua altura é {altura} e seu peso é {peso}, então seu IMC é {IMC:F2}");
