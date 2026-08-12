274748f3a18b71934431f9cdeabc0dd335f03342
FellipeParaguassu <8rrfellipeparaguassu@gmail.com>
2026-08-12 00:12:26 -0400

    Corrige conteúdo do Exercicio 4

--- C#/ExerciciosFaculdade/Exercicio4/Exercicio 4/Program.cs
+++ C#/ExerciciosFaculdade/Exercicio4/Exercicio 4/Program.cs
@@ -1,5 +1,7 @@
-﻿//Conversor Celsius para Fahreinheit
-Console.WriteLine("Sistema de Celsius para Fahrenheit \nQuantos graus Celsius?");
-double celsius = double.Parse(Console.ReadLine());
-double fahrenheit = (celsius * 9/5) + 32;
-Console.WriteLine($"{celsius:F2} graus Celsius equivalem a {fahrenheit:F2} graus Fahrenheit.");
+﻿//Programa que calcula a média de gasolina gasta com base na distância percorrida e no consumo do veículo.
+Console.WriteLine("Digite a distância percorrida em quilômetros:");
+double distancia = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite o consumo do veículo em quilômetros por litro:");
+double consumo = double.Parse(Console.ReadLine());
+double mediaGasolina = distancia / consumo;
+Console.WriteLine($"A média de gasolina gasta é de {mediaGasolina:F2} litros.");
