6558d7d075eaa3d2a1c62072654cb5977eb25205
FellipeParaguassu <8rrfellipeparaguassu@gmail.com>
2026-08-11 23:52:29 -0400

    Adiciona Exercicio 6

---
+++ C#/ExerciciosFaculdade/Exercicio6/Program.cs
@@ -0,0 +1,5 @@
+//PROGRAMA QUE CONVERTE CELSIUS PARA FAHREINHEIT
+Console.WriteLine("Coloque quantos graus celsius você quer converter");
+double celsius = Console.ReadLine();
+double fahreiheit = (celsius * 9 / 5) + 32;
+Console.WriteLine($"Certo, aqui está, {celsius} graus celsius é igual à {fahreiheit} graus fahreiheit ");
