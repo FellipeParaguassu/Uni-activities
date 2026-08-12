542a434dc37cbd876b4d51e0fcf6e078de4dd38e
FellipeParaguassu <8rrfellipeparaguassu@gmail.com>
2026-08-12 01:11:47 -0400

    Finaliza Exercicio 8

--- C#/ExerciciosFaculdade/Exercicio8/Program.cs
+++ C#/ExerciciosFaculdade/Exercicio8/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Programa que informa o troco de um compra
+Console.WriteLine("Informe qual o valor da compra:");
+double valor = double.Parse((Console.ReadLine()));
+Console.WriteLine("Informe quanto de dinheiro foi dado para o pagamento");
+double dinheiro_dado = double.Parse(Console.ReadLine());
+if (dinheiro_dado >= valor)
+{
+    double troco = dinheiro_dado - valor;
+    Console.WriteLine("O troco é: " + troco);
+}
+else
+{
+    Console.WriteLine("Dinheiro insuficiente.");
+}
